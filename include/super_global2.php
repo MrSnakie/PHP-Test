aef4c2d37d8fc4490bc1112b2979ea0952ccec35
MrSnakie <petitloulou96@free.fr>
2017-03-09 10:04:52 +0100

    Exo 8.2

---
+++ include/super_global2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Starting PHP</title>
+	<meta name="author" content="Stéphane Bariller">
+	<link rel="stylesheet" type="text/css" href="../style/css/style.css">
+</head>
+
+<body>
+	<container>
+		<h2>Exercice 2</h2>
+		<?php
+			session_start();
+
+			echo "<p>" .$_SESSION['nom']. "</p>";
+			echo "<p>" .$_SESSION['prenom']. "</p>";
+			echo "<p>" .$_SESSION['age']. "</p>";
+		?>
+		<p><a href='../super_global.php'>Retour</a></p>
+	</container>
+</body>
+</html>
--- super_global.php
+++ super_global.php
@@ -8,9 +8,23 @@
 
 <body>
 	<container>
+		<h2>Exercice 1</h2>
 		<p><strong>Votre "user agent":</strong> <?php echo $_SERVER['HTTP_USER_AGENT']; ?></p>
 		<p><strong>Votre adresse IP:</strong> <?php echo $_SERVER['REMOTE_ADDR']; ?></p>
 		<p><strong>Nom du serveur:</strong> <?php echo $_SERVER['SERVER_NAME']; ?></p>
+
+		<hr>
+
+		<h2>Exercice 2</h2>
+		<?php
+			session_start();
+
+			$_SESSION['nom'] = "L'Éponge";
+			$_SESSION['prenom'] = "Bob";
+			$_SESSION['age'] = "10 ans";
+			
+			echo "<p><a href='include/super_global2.php'>Sessions PHP</a></p>";
+		?>
 	</container>
 </body>
 </html>
