531eeaf7d89fc962abe4361cdb53aebb6cf2ec0a
MrSnakie <petitloulou96@free.fr>
2017-03-14 11:23:16 +0100

    Exo 10.1

--- class.php
+++ class.php
@@ -1,5 +1,7 @@
 <?php
 	require_once 'include/config.php';
+	$traitement_clients = $pdo->query('SELECT * FROM clients ORDER BY lastName');
+	$clients = $traitement_clients->fetchAll();
 ?>
 
 <!DOCTYPE html>
@@ -13,9 +15,58 @@
 <body>
 	<container>
 		<h2>Exercice n°1</h2>
-		<?php
-			// Afficher tous les clients
-		?>
+		<table align="center">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Nom</th>
+					<th>Prénom</th>
+					<th>Date de naissance</th>
+					<th>Carte</th>
+					<th>Numéro de carte</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+					/* Afficher tous les clients */
+					foreach  ($clients as $value) {
+						echo '<tr>';
+							echo '<td>';
+								print_r($value->id); // ID
+							echo '</td>';
+
+							echo '<td>';
+								print_r($value->lastName); // Nom
+							echo '</td>';
+
+							echo '<td>';
+								print_r($value->firstName); // Prénom
+							echo '</td>';
+
+							echo '<td style="width: 150px">';
+								print_r($value->birthDate); // Date de naissance
+							echo '</td>';
+
+							echo '<td>';
+								if($value->card == 1){ // Carte
+									echo 'Oui';
+								}else{
+									echo 'Non';
+								};
+							echo '</td>';
+
+							echo '<td>';
+								if($value->cardNumber == NULL){ // Numéro de carte
+									echo 'Aucun';
+								}else{
+									print_r($value->cardNumber);
+								};
+							echo '</td>';
+						echo '</tr>';
+					}
+				?>
+			</tbody>
+		</table>
 
 		<hr>
 		<p><a href='index.php'>Retour</a></p>
